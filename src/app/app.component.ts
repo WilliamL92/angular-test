db45701242bb586d547871d66cca958faf4b0073
William <lavit.william@gmail.com>
2025-03-31 18:01:57 +0200

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,9 @@ import { environment } from './../environments/environment.development';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = environment.title;
   apiData: any;
 
   constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    // Remplacez '/endpoint' par le chemin de votre API .NET
-    this.http.get(`${environment.apiUrl}/api/users`).subscribe({
-      next: (data) => {
-        this.apiData = data;
-        console.log('Données récupérées :', this.apiData);
-      },
-      error: (error) => {
-        console.error('Erreur lors de la récupération des données');
-      }
-    });
-  }
 }
